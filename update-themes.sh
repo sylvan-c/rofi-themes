e21e3a81b066e7c52e071b3f6d012cf237463102
Sylvan <sylvanchouhan@gmail.com>
2021-09-11 11:29:37 +0100

    Make updating script more user friendly

--- update-themes.sh
+++ update-themes.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-for file in /home/$USER/.config/rofi/themes/*; do
-    sudo cp $file /usr/share/rofi/themes
+theme_dir=/home/$USER/.config/rofi/themes
+target_dir=/usr/share/rofi/themes
+
+for file in $theme_dir/*; do
+    sudo cp -u $file $target_dir && echo "Updating $(echo $file | sed 's:.*/::')"
 done
+
+echo "Themes up to date"
